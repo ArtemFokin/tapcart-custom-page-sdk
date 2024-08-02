fdfc13de7992d1318fb52e8dccfbd550dd771d56
Artem Fokin <artfokin69@gmail.com>
2024-08-02 21:08:27 +0000

    timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log({
+        tapcart: window.Tapcart,
+      });
+      setState((v) => v + 1);
+    }, 1000);
+  }, []);
   return (
     <div className="App">
-      <h1>Tapcart device: {window.Tapcart?.device.id}</h1>
+      <h1>
+        Tapcart device: {window.Tapcart?.device.id} - {state}
+      </h1>
     </div>
   );
 }
